44a65ce94f165a535cef1915ee6a4994ca29e827
Steve Jones <sjones@yodle.com>
2015-09-07 13:02:38 -0400

    more complete idea logic flow.. untested yet

--- index.js
+++ index.js
@@ -1,32 +1,70 @@
-var R = require('ramda');
+var r = require('ramda');
 var q = require('q');
 
-var promiseFlow = function promiseFlow () { // Variadic. Args are ordered fns used to build promise chain.
-	var pChain = q(null);
-	R.map(function mapPromises(nextLinkVal) {
-		var nextPromiseLink = R.ifElse(R.is(Function), R.identity, R.always);
-		pChain = pChain.then(nextPromiseLink(nextLinkVal));
-	}, Array.prototype.slice.call(arguments));
-	return pChain;
+var args2List = Array.prototype.slice.call;
+
+var promiseFlow = function promiseFlow (pChainList) {
+	return r.reduce(q.when, q(null), pChainList);
 };
 
-var identityFlow = function (entity) {
-	R.ifElse(R.is(Function), R.identity, R.always);
+var nextThenPLink = r.ifElse(r.is(Function), r.identity, r.always);
+
+var identityFlo = function (form) {
+	r.ifElse(r.is(Function), r.identity, r.always);
 };
 
-var flowForms = [
-	[
-		R.either(R.is(Function), R.complement(R.isArrayLike)),
-		identityFlow
-	]
+var isCondFlo     = r.pipe(r.nth(0), r.isArrayLike);
+var isPartialFlo  = r.pipe(r.nth(0), r.is(Function));
+
+var condFlo = function (form) {
+
+};
+
+var partialFlo = function () {
+	var args = args2List(arguments);
+	return function () {
+		return q.all(mapListToPromises(r.tail(args))).spread(r.head(args));
+	};
+};
+
+var specialFormFlo = r.cond([
+	[isPartialFlo, partialFlo],
+	[isCondFlo, condFlo]
+]);
+
+var isSpecialFormKey = r.equals('*');
+var isSpecialFormFlo = r.both(r.isArrayLike, r.pipe(r.nth(0), isSpecialFormKey));
+var isIdentityFlo    = r.either(r.is(Function), r.complement(isSpecialFormFlo));
+
+var floForms = [
+	[isIdentityFlo, identityFlo],
+	[isSpecialFormFlo, r.pipe(r.tail, specialFormFlo)]
 ];
 
-var lazyFlowForms = R.map(R.pipe(R.nth(1), R.partial(R.partial)));
+var Pflo = r.map(r.cond(floForms));
 
-var FormFlowEvalSwitch = R.cond(lazyFlowForms(flowForms));
+var _export = function () {
+	var pfloList = args2List(arguments);
 
 
 
+
+
+	return q.Promise(function (res, rej) {
+		var pflo = Pflo(pfloList);
+
+
+		//var lazyEvalFloList = function () {
+		//
+		//};
+		//
+		//var floListEvalComplete = function (err, data) {
+		//
+		//};
+		//
+		//a.eachLimit(floList, 1, );
+	});
+};
 
 
 
